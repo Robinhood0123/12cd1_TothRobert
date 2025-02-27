1268706ef916762b10806decf8500becbc244eb5
Robinhood0123 <93657599+Robinhood0123@users.noreply.github.com>
2025-02-27 09:15:32 +0100

    GET hiba javítása
    app.get("/",(req, res) => {
        res.send("Mukodik a szerver.")
    })

--- teliolimpia.js
+++ teliolimpia.js
@@ -5,6 +5,10 @@ const bodyparser = require('body-parser')
 
 app.use(bodyparser.json())
 
+app.get("/",(req, res) => {
+    res.send("Mukodik a szerver.")
+})
+
 app.listen(3000, () =>  {
     console.log('A teli olimpia szervere a 3000-es porton fut.')
 }) 
